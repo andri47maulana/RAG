e00b64f17a6e76a51090c99e2c756b6c73f7607f
andri47maulana <andri47maulana@gmail.com>
2025-08-20 17:58:06 +0700

    update server.js .env

--- frontend/server.js
+++ frontend/server.js
@@ -7,11 +7,24 @@ const port = process.env.NODE_PORT;
 
 
 
+
+// Serve upload.html with injected env
+app.get('/upload.html', (req, res) => {
+  const fs = require('fs');
+  let html = fs.readFileSync(path.join(__dirname, 'pages', 'upload.html'), 'utf8');
+  // Inject window.env script after <body>
+  html = html.replace(
+    '<body>',
+    `<body><script>window.env = { PYTHON_URL: "${process.env.PYTHON_URL}", SECURITY_KEY: "${process.env.SECURITY_API_KEY}" };</script>`
+  );
+  res.send(html);
+});
+
 app.use(express.static(path.join(__dirname, 'pages')));
-app.use(express.json());
+app.use(express.json()); 
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'pages', 'upload.html'));
+  res.redirect('/upload.html');
 });
 
 app.listen(port, () => {
